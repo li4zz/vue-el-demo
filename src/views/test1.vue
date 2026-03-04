61ef95364250def374beb9dc4dcf93a9bdb40cb4
licheng <13692176069@163.com>
2026-03-04 16:22:17 +0800

    update

--- src/views/test1.vue
+++ src/views/test1.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="test1"></div>
+  <div class="test1">
+    <div class="box">
+      <div class="a">
+        <div class="b"><img src="@/assets/images/5.png" /></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,17 +13,53 @@ export default {
   components: {},
   props: {},
   data() {
-    return {
-    };
+    return {};
   },
   watch: {},
   computed: {},
   methods: {},
   created() {},
-  mounted() {}
+  mounted() {},
 };
 </script>
-<style lang="less"
- scoped>
-.test1{}
+<style>
+.box {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.a {
+  width: 200px;
+  height: 260px;
+  border-radius: 0 0 110px 110px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  overflow: hidden;
+  transition: all 0.5s;
+}
+.b {
+  width: 200px;
+  height: 200px;
+  box-sizing: border-box;
+  background-color: rgb(205, 205, 255);
+  border: 2px solid #000;
+  border-radius: 50%;
+}
+img {
+  width: 100%;
+  height: 100%;
+  transition: all 0.5s;
+}
+.a:hover {
+  transform: scale(0.9);
+  transition: all 0.5s;
+}
+img:hover {
+  transform: scale(1.5);
+  transition: all 0.5s;
+}
 </style>
